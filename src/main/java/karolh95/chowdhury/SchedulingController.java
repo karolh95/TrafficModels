9fe73a5bd465690cd48287ced40f221a3f131899
karolh95 <karolh95@live.com>
2020-08-26 17:15:40 +0200

    Created scheduling controller

---
+++ src/main/java/karolh95/chowdhury/SchedulingController.java
@@ -0,0 +1,29 @@
+package karolh95.chowdhury;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import karolh95.chowdhury.service.SchedulingService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("task")
+@RequiredArgsConstructor
+public class SchedulingController {
+
+	private final SchedulingService schedulingService;
+
+	@GetMapping("run")
+	public void run() {
+
+		schedulingService.runScheduledTask();
+	}
+
+	@GetMapping("pause")
+	public void pause() {
+
+		schedulingService.cancelScheduledTask();
+	}
+}

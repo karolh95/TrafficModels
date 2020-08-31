9694d5e571188dcc64f5bc9aa613982079a5bd0e
karolh95 <karolh95@live.com>
2020-08-31 12:30:05 +0200

    Removed unnecessary configuration

--- src/main/java/karolh95/chowdhury/WebSocketConfig.java
+++ src/main/java/karolh95/chowdhury/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package karolh95.chowdhury;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -9,13 +8,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-
-		registry.enableSimpleBroker("/counter");
-		registry.setApplicationDestinationPrefixes("/chowdhury");
-	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {

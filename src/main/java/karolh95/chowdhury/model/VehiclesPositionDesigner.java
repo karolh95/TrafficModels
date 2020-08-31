8a2262d374589bea3853050850f1ca07da07450e
karolh95 <karolh95@live.com>
2020-08-31 14:11:18 +0200

    Created Vehicle Position Designer

---
+++ src/main/java/karolh95/chowdhury/model/VehiclesPositionDesigner.java
@@ -0,0 +1,67 @@
+package karolh95.chowdhury.model;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import org.springframework.stereotype.Component;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Component
+@RequiredArgsConstructor
+public class VehiclesPositionDesigner {
+
+	private final Road road;
+
+	@Setter(value = AccessLevel.PRIVATE)
+	private int laneLength;
+
+	private int laneNumber;
+	private int position;
+	private int gap;
+
+	public void placeVehicles(List<Vehicle> vehicles) {
+
+		setLaneLength(road.getLanesLength());
+		calculateGap(vehicles.size());
+		resetPosition();
+		shuffle(vehicles);
+
+		for (Vehicle vehicle : vehicles) {
+
+			road.set(vehicle, laneNumber, position);
+
+			vehicle.setLane(laneNumber);
+			vehicle.setPosition(position);
+
+			nextPosition();
+		}
+	}
+
+	private void calculateGap(int vehiclesNumber) {
+
+		gap = road.getCellsPerVehicle(vehiclesNumber);
+	}
+
+	private void resetPosition() {
+
+		laneNumber = 0;
+		position = 0;
+	}
+
+	private void shuffle(List<Vehicle> vehicles) {
+
+		Collections.shuffle(vehicles, new Random());
+	}
+
+	private void nextPosition() {
+
+		position += gap;
+
+		if (position > laneLength) {
+			position -= laneLength;
+			laneNumber++;
+		}
+	}
+}

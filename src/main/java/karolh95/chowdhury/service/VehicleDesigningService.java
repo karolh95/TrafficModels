6364dbb48a3f1ce4ac806f387586c2457c1c3e75
karolh95 <karolh95@live.com>
2020-09-02 09:45:42 +0200

    'setVehicles' method now returns empty list instead of null

--- src/main/java/karolh95/chowdhury/service/VehicleDesigningService.java
+++ src/main/java/karolh95/chowdhury/service/VehicleDesigningService.java
@@ -1,5 +1,6 @@
 package karolh95.chowdhury.service;
 
+import java.util.Collections;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import karolh95.chowdhury.model.Model;
@@ -21,7 +22,7 @@ public class VehicleDesigningService {
 		if (areValid) {
 			return designer.setVehicles(descriptors);
 		} else {
-			return null;
+			return Collections.emptyList();
 		}
 	}
 }

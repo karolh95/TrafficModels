e729d5eed7dd1149d0b7e78de3e638e02a7693b2
karolh95 <karolh95@live.com>
2020-10-12 17:15:53 +0200

    Added update logic of the ASEP model.

--- src/main/java/karolh95/chowdhury/model/ASEP.java
+++ src/main/java/karolh95/chowdhury/model/ASEP.java
@@ -1,6 +1,7 @@
 package karolh95.chowdhury.model;
 
 import karolh95.chowdhury.model.component.Road;
+import karolh95.chowdhury.model.component.Vehicle;
 
 public class ASEP extends Model {
 
@@ -8,5 +9,18 @@ public class ASEP extends Model {
 
 	public ASEP(Road road) {
 		super(road);
+	}
+
+	@Override
+	protected void beforeVehiclePositionUpdate(Vehicle vehicle) {
+
+		int velocity = isNextCellEmpty(vehicle) ? 0 : MAX_VELOCITY;
+
+		vehicle.setVelocity(velocity);
+	}
+
+	private boolean isNextCellEmpty(Vehicle vehicle) {
+
+		return isEmpty(vehicle.getLane(), vehicle.getPosition() + 1);
 	}
 }

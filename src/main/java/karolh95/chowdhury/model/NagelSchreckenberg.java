c28260be4f3b4f6c63ea96e0cd63e02053a14d00
karolh95 <karolh95@live.com>
2020-10-25 11:12:48 +0100

    Removed unnecessary comments from NagelSchreckenberg model

--- src/main/java/karolh95/chowdhury/model/NagelSchreckenberg.java
+++ src/main/java/karolh95/chowdhury/model/NagelSchreckenberg.java
@@ -3,16 +3,6 @@ package karolh95.chowdhury.model;
 import karolh95.chowdhury.model.component.Road;
 import lombok.Setter;
 
-/**
- * <li>if V(n) < Vmax, then V(n) = V(n) + 1,</li>
- * <li>if V (n) > ( A X : (n) -- 1), then V(n) = dX(n) - 1,</li>
- * <li>if V(n) > 0 and ran f< Pd, then V(n) = V(n) - 1,</li>
- * <li>X(n) = X(n) + V(n),</li>
- * </ol>
- * where {@code Pd} is the probability of a random deceleration and
- * {@code ran f} is a random fraction uniformly distributed between 0 and 1.
- * </p>
- **/
 @Setter
 public class NagelSchreckenberg extends Model {
 
